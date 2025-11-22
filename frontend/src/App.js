866ab7a2808f8ff5c93508b787b7e46c2a491101
Ananya M D <ananyamaniyana@gmail.com>
2025-11-22 13:59:30 +0530

    Move dashboard.css to frontend/styles and update import; remove old src/styles file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
-import "./styles/dashboard.css";
+import "../styles/dashboard.css";
 
 function App() {
   return <Dashboard />;
