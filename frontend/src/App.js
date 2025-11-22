59eefff1d103e7d17b8bb588f935cfcee84a23f5
Ananya M D <ananyamaniyana@gmail.com>
2025-11-22 14:15:07 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
-import "../styles/dashboard.css";
+import "./styles/dashboard.css";
 
 function App() {
   return <Dashboard />;
 }
 
 export default App;
+
